c6ca78d2b9a5573799e18584be274e34ec4432cb
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-02-15 11:35:47 +0100

    Added uppercase for all computer results

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function getComputerChoice()
     let select_item = Math.floor(Math.random() * 3);
     if (select_item > 2)
         select_item = 2;
-    return (array_of_plays[select_item]);
+    return (array_of_plays[select_item].toUpperCase());
 }
 
 console.log(getComputerChoice());
